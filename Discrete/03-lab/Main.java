197ee13e5c4fe31f73be72c051e8b0f012449127
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-04-08 14:51:49 +0700

    fix(*): input from user

--- Discrete/03-lab/Main.java
+++ Discrete/03-lab/Main.java
@@ -4,9 +4,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // System.out.print("Enter your equation: ");
-        // String equation = scanner.nextLine();
-        String equation = "5 + 2 / 4 * 6";
+        System.out.print("Enter your equation: ");
+        String equation = scanner.nextLine();
         scanner.close();
 
         Notation polishNotation = new Notation(equation);
