fba0ba11db3d3d326040f977ebf5dc82fc94fbaf
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-07-06 20:23:37 +0700

    :sparkles: (lab) add lab 1.1

---
+++ Operating Systems/01-lab/Lab1_1Ans_65437.bat
@@ -0,0 +1,8 @@
+@REM 65050437 Noppakorn Kaewsalabnil
+
+@REM copy sub\test1.txt
+@REM copy c:\oslab1\sub\test1.txt c:\oslab1\sub\test2.txt
+@REM md c:\oslab1\abc
+@REM del c:\oslab1\abc\test2.txt
+@REM rd c:\oslab1\sub /s /q
+@REM c:\windows\system32
