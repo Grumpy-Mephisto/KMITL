c6e67d958913a52d189710100116bdb3232abc4b
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-07-20 17:25:06 +0700

    feat(os): add lab 3
    on 2 files is same program <:

---
+++ Operating Systems/03-lab/lab3q4_65437.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  int a, b, c;
+  printf("Enter a numbers: ");
+  scanf("%d", &a);
+
+  printf("Enter two numbers: ");
+  scanf("%d %d", &b, &c);
+
+  printf("Summation = %d\n", a + b + c);
+
+  return 0;
+}
