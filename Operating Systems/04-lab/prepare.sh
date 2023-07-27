74be1e3c5e6487ef569167b5a3dfb96074c261f7
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-07-27 13:44:37 +0700

    fix(os): update prepare for create file all tasks

--- Operating Systems/04-lab/prepare.sh
+++ Operating Systems/04-lab/prepare.sh
@@ -8,5 +8,10 @@
 [[ -f edit1.txt ]] || { echo "Creating file edit1.txt"; touch edit1.txt; }
 [[ -f edit2.txt ]] || { echo "Creating file edit2.txt"; ls -l > edit2.txt; }
 
+# Loop create file q1-q11.sh
+for i in {1..11}; do
+    [[ -f q$i.sh ]] || { echo "Creating file q$i.sh"; touch q$i.sh; }
+done
+
 # Check
 ls -l
