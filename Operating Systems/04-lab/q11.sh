f65183e22ac4147df2ffc9f4ad129242fddbac05
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-07-27 15:40:53 +0700

    style(os): update color on q11

--- Operating Systems/04-lab/q11.sh
+++ Operating Systems/04-lab/q11.sh
@@ -5,6 +5,11 @@ CORRECT_ANSWER=10
 WRONG_ANSWER=7
 NOT_COMPILED=5
 
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+RESET=$(tput sgr0)
+
 for file in L*.c; do
     if [ -f "$file" ]; then
         program_name=$(echo "$file" | cut -d '.' -f 1)
@@ -13,12 +18,12 @@ for file in L*.c; do
             answer=$(./"$program_name")
 
             if [ "$answer" -eq "$RESULT" ]; then
-                printf "%s %s\n" "$program_name" "$CORRECT_ANSWER"
+                printf "%s\t%s%d%s\n" "$program_name" "$GREEN" "$CORRECT_ANSWER" "$RESET"
             else
-                printf "%s %s\n" "$program_name" "$WRONG_ANSWER"
+                printf "%s\t%s%d%s\n" "$program_name" "$YELLOW" "$WRONG_ANSWER" "$RESET"
             fi
         else
-            printf "%s %s\n" "$program_name" "$NOT_COMPILED"
+            printf "%s\t%s%d%s\n" "$program_name" "$RED" "$NOT_COMPILED" "$RESET"
         fi
     fi
 done
