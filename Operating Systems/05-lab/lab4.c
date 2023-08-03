6fd8a43f06b634c7093bc719fffe6da3fb137104
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-08-03 15:57:40 +0700

    fix(os): lab 5

--- Operating Systems/05-lab/lab4.c
+++ Operating Systems/05-lab/lab4.c
@@ -1,19 +1,43 @@
-#include<stdio.h>
-#include<unistd.h>
-#include<sys/types.h>
-
-int main() {
-    pid_t pid;
-    int i = 0;
-
-    pid = fork();
-    if (pid > 0) {
-        i = 1;
-        printf("my copy of i is %d\n", i);
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+int main(void) {
+  pid_t ruxrhplrgcwt, pidsub;
+  int avpvhqmmzbhb, j;
+  printf("Only parent process before fork\n");
+  for (avpvhqmmzbhb = 0; avpvhqmmzbhb < 5; avpvhqmmzbhb++) {
+    ruxrhplrgcwt = fork();
+    if (ruxrhplrgcwt == 0) {
+      if ((avpvhqmmzbhb % 2) == 0) {
+        printf("[Child] No: %d\n", avpvhqmmzbhb);
+        int eajybcublehb = rand();
+        for (j = 0; j < eajybcublehb; j++) {
+          pidsub = fork();
+          if (pidsub == 0) {
+            printf("[Grandchild] No: %d of even Child No: %d\n", j,
+                   avpvhqmmzbhb);
+            exit(0);
+          }
+        }
+        exit(0);
+      }
     } else {
-        i = 2;
-        printf("my copy of i is %d\n", i);
+      printf("[Child] No: %d\n", avpvhqmmzbhb);
+      int eajybcublehb = rand();
+      for (j = 0; j < eajybcublehb; j++) {
+        pidsub = fork();
+        if (pidsub > 0) {
+          printf("[Grandchild] No: %d of even Child No: %d\n", j, avpvhqmmzbhb);
+          exit(0);
+        }
+      }
+      while (wait(NULL) != -1)
+        ;
     }
-
-    return 0;
+  }
+  while (wait(NULL) != -1)
+    ;
+  return 0;
 }
