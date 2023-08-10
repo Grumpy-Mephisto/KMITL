d6b2c53558161291ed8ef89c1a209dd2740715e3
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-08-10 14:29:40 +0700

    feat(dsa): add lab 4

---
+++ Data Structures and Algorithm/04-lab/LinkedListTester.java
@@ -0,0 +1,9 @@
+public class LinkedListTester {
+    public static void main(String[] args) {
+        MyLinkedList mList = new MyLinkedList();
+        mList.add(4);
+        mList.add(3);
+        mList.add(7);
+        System.out.println(mList.toString());
+    }
+}
---
+++ Data Structures and Algorithm/04-lab/MyLinkedList.java
@@ -0,0 +1,86 @@
+public class MyLinkedList {
+    public class Node {
+        int data;
+        Node next;
+        
+        public Node(int d) {
+            data = d;
+        }
+    }
+
+    Node head = null;
+
+    public int getAt(int i) {
+        Node p = head;
+        while(i > 0) {
+            p = p.next;
+            i--;
+        }
+
+        return p.data;
+    }
+
+    public int setAt(int d, int i) {
+        Node p = head;
+        while(i > 0) {
+            p = p.next;
+            i--;
+        }
+        return p.data = d;
+    }
+    
+    public void add (int d) {
+        Node p = new Node(d);
+        p.next = head;
+        head = p;
+    }
+    
+    public void insert (int d) {
+        Node p = new Node(d);
+        p.next = head;
+        head = p;
+    }
+    
+    public void find (int d) {
+        Node p = head;
+        while(p != null) {
+            if (p.data == d) {
+                System.out.println("Found " + d);
+                return;
+            }
+            p = p.next;
+        }
+        System.out.println("Not found " + d);
+    }
+
+    public void delete (int d) {
+        Node p = head;
+        Node prev = null;
+        while(p != null) {
+            if (p.data == d) {
+                if (prev == null) {
+                    head = p.next;
+                } else {
+                    prev.next = p.next;
+                }
+                return;
+            }
+            prev = p;
+            p = p.next;
+        }
+    }
+
+    public String toString() {
+        StringBuffer sb = new StringBuffer();
+        Node p = head;
+
+        while(p != null) {
+            sb.append("→ [");
+            sb.append(p.data);
+            sb.append("] ");
+            p = p.next;
+        }
+        sb.append("→ null");
+        return new String(sb);
+    }
+}
