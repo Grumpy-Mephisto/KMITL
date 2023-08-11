f08a218e0671dc5925e0bfec1e4a26cb90aff1bd
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-08-11 10:33:41 +0700

    :art: (dsa) prettier code

--- Data Structures and Algorithm/04-lab/LinkedListTester.java
+++ Data Structures and Algorithm/04-lab/LinkedListTester.java
@@ -1,33 +1,33 @@
 public class LinkedListTester {
-    public static void main(String[] args) {
-        MyLinkedList mList = new MyLinkedList();
-        
-        System.out.println("===== Adding =====");
-        mList.add(4);
-        mList.add(3);
-        mList.add(7);
-        System.out.println(mList.toString());
+  public static void main(String[] args) {
+    MyLinkedList mList = new MyLinkedList();
 
-        System.out.println("===== Inserting =====");
-        mList.insert(5);
-        mList.insert(2);
-        mList.insert(8);
-        System.out.println(mList.toString());
+    System.out.println("===== Adding =====");
+    mList.add(4);
+    mList.add(3);
+    mList.add(7);
+    System.out.println(mList.toString());
 
-        System.out.println("===== Finding =====");
-        mList.find(5);
-        mList.find(2);
-        mList.find(8);
-        mList.find(9);
-        System.out.println(mList.toString());
+    System.out.println("===== Inserting =====");
+    mList.insert(5);
+    mList.insert(2);
+    mList.insert(8);
+    System.out.println(mList.toString());
 
-        System.out.println("===== Deleting =====");
-        mList.delete(5);
-        mList.delete(2);
-        mList.delete(8);
-        mList.delete(9);
-        System.out.println(mList.toString());
-        
-        System.out.println(mList.toString());
-    }
+    System.out.println("===== Finding =====");
+    mList.find(5);
+    mList.find(2);
+    mList.find(8);
+    mList.find(9);
+    System.out.println(mList.toString());
+
+    System.out.println("===== Deleting =====");
+    mList.delete(5);
+    mList.delete(2);
+    mList.delete(8);
+    mList.delete(9);
+    System.out.println(mList.toString());
+
+    System.out.println(mList.toString());
+  }
 }
--- Data Structures and Algorithm/04-lab/MyLinkedList.java
+++ Data Structures and Algorithm/04-lab/MyLinkedList.java
@@ -1,110 +1,108 @@
 public class MyLinkedList {
-    public class Node {
-        int data;
-        Node next;
-        
-        public Node(int d) {
-            data = d;
+  public class Node {
+    int data;
+    Node next;
+
+    public Node(int d) { data = d; }
+  }
+
+  Node head = null;
+
+  public int getAt(int i) {
+    Node p = head;
+    while (i > 0) {
+      p = p.next;
+      i--;
+    }
+
+    return p.data;
+  }
+
+  public int setAt(int d, int i) {
+    Node p = head;
+    while (i > 0) {
+      p = p.next;
+      i--;
+    }
+    return p.data = d;
+  }
+
+  public int size() {
+    int count = 0;
+    Node p = head;
+    while (p != null) {
+      count++;
+      p = p.next;
+    }
+    return count;
+  }
+
+  public void add(int d) {
+    Node p = new Node(d);
+    p.next = head;
+    head = p;
+  }
+
+  public void insert(int d) {
+    Node q = new Node(d);
+    Node p = head;
+    while (p.next != null) {
+      if (p.next.data > d) {
+        if (p == head) {
+          q.next = head;
+          head = q;
+        } else {
+          q.next = p.next;
+          p.next = q;
         }
+        return;
+      }
+
+      p = p.next;
     }
+    p.next = q;
+  }
 
-    Node head = null;
+  public void find(int d) {
+    Node p = head;
+    while (p != null) {
+      if (p.data == d) {
+        System.out.println("Found " + d);
+        return;
+      }
+      p = p.next;
+    }
+    System.out.println("Not found " + d);
+  }
 
-    public int getAt(int i) {
-        Node p = head;
-        while(i > 0) {
-            p = p.next;
-            i--;
+  public void delete(int d) {
+    Node p = head;
+    Node prev = null;
+    while (p != null) {
+      if (p.data == d) {
+        if (prev == null) {
+          head = p.next;
+        } else {
+          prev.next = p.next;
         }
+        return;
+      }
+      prev = p;
+      p = p.next;
+    }
+  }
 
-        return p.data;
-    }
+  public String toString() {
+    StringBuffer sb = new StringBuffer();
+    Node p = head;
 
-    public int setAt(int d, int i) {
-        Node p = head;
-        while(i > 0) {
-            p = p.next;
-            i--;
-        }
-        return p.data = d;
+    while (p != null) {
+      sb.append("→ [");
+      sb.append(p.data);
+      sb.append("] ");
+      p = p.next;
     }
-
-    public int size() {
-        int count = 0;
-        Node p = head;
-        while(p != null) {
-            count++;
-            p = p.next;
-        }
-        return count;
-    }
-    
-    public void add (int d) {
-        Node p = new Node(d);
-        p.next = head;
-        head = p;
-    }
-    
-    public void insert (int d) {
-        Node q = new Node(d);
-        Node p = head;
-        while(p.next != null) {
-            if (p.next.data > d) {
-                if (p == head) {
-                    q.next = head;
-                    head = q;
-                } else {
-                    q.next = p.next;
-                    p.next = q;
-                }
-                return;
-            }
-
-            p = p.next;
-        }
-        p.next = q;
-    }
-    
-    public void find (int d) {
-        Node p = head;
-        while(p != null) {
-            if (p.data == d) {
-                System.out.println("Found " + d);
-                return;
-            }
-            p = p.next;
-        }
-        System.out.println("Not found " + d);
-    }
-
-    public void delete (int d) {
-        Node p = head;
-        Node prev = null;
-        while(p != null) {
-            if (p.data == d) {
-                if (prev == null) {
-                    head = p.next;
-                } else {
-                    prev.next = p.next;
-                }
-                return;
-            }
-            prev = p;
-            p = p.next;
-        }
-    }
-
-    public String toString() {
-        StringBuffer sb = new StringBuffer();
-        Node p = head;
-
-        while(p != null) {
-            sb.append("→ [");
-            sb.append(p.data);
-            sb.append("] ");
-            p = p.next;
-        }
-        sb.append("→ null");
-        return new String(sb);
-    }
+    sb.append("→ null");
+    return new String(sb);
+  }
 }
