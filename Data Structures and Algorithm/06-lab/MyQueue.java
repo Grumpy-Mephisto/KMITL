8008e14f6a7141a20037e7f0c70445fb8f9eeac4
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-08-23 21:01:50 +0700

    docs(param): add param

--- Data Structures and Algorithm/06-lab/MyQueue.java
+++ Data Structures and Algorithm/06-lab/MyQueue.java
@@ -1,12 +1,46 @@
 public interface MyQueue<E> {
+    /**
+     * Inserts the specified element into this queue if it is possible to do so
+     * 
+     * @param e the element to add
+     * @throws IllegalStateException if the element cannot be added at this time due to capacity
+     *         restrictions
+     * @throws NullPointerException if the specified element is null and this queue does not permit
+     *         null elements
+     * @throws IllegalArgumentException if some property of this element prevents it from being
+     *         added to this queue
+     * @return true if the element was added to this queue, else false
+     */
     void enqueue(E e);
 
+    /**
+     * Retrieves and removes the head of this queue
+     * 
+     * @throws NoSuchElementException if this queue is empty
+     * @return the head of this queue
+     */
     E dequeue();
 
+    /**
+     * Retrieves, but does not remove, the head of this queue
+     * 
+     * @throws NoSuchElementException if this queue is empty
+     * @return the head of this queue
+     */
     E front();
 
+    /**
+     * Returns true if this queue is full
+     * 
+     * @return true if this queue is full
+     */
     boolean isFull();
 
+    /**
+     * Returns true if this queue is empty
+     * 
+     * @return true if this queue is empty
+     */
     boolean isEmpty();
 
     int size();
--- Data Structures and Algorithm/06-lab/MyStack.java
+++ Data Structures and Algorithm/06-lab/MyStack.java
@@ -1,11 +1,40 @@
 public interface MyStack<E> {
+    /**
+     * Pushes an element onto the top of this stack
+     * 
+     * @param element the element to be pushed onto this stack
+     * @throws NullPointerException if the specified element is null
+     */
     void push(E element);
 
+    /**
+     * Removes the element at the top of this stack and returns that element as the value of this
+     * function
+     * 
+     * @throws NoSuchElementException if this stack is empty
+     * @return the element at the top of this stack
+     */
     E pop();
 
+    /**
+     * Looks at the element at the top of this stack without removing it from the stack
+     * 
+     * @throws NoSuchElementException if this stack is empty
+     * @return the element at the top of this stack
+     */
     E top();
 
+    /**
+     * Returns true if this stack is full
+     * 
+     * @return true if this stack is full
+     */
     boolean isEmpty();
 
+    /**
+     * Returns true if this stack is empty
+     * 
+     * @return true if this stack is empty
+     */
     int size();
 }
