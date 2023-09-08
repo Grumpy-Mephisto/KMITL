f76a68a75e1b7b369ace21e008635e30844a78cf
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-09-08 18:17:14 +0700

    fix(os): update signal infinity loop

--- Operating Systems/08-lab/08-lab-a/lab8a_q3.c
+++ Operating Systems/08-lab/08-lab-a/lab8a_q3.c
@@ -8,7 +8,7 @@
 int not_done = 1;
 volatile int count = 0;
 
-void mySIGALRMhandler(int sig) {
+void mySIGhandler(int sig) {
   signal(SIGALRM, SIG_IGN);
   not_done = 0;
   printf("SIGALRM received\n");
@@ -21,8 +21,7 @@ void mySIGKILLhandler(int sig) {
 }
 
 int main(int argc, char const *argv[]) {
-  // signal(SIGALRM, mySIGALRMhandler);
-  signal(SIGKILL, mySIGKILLhandler);
+  signal(SIGALRM, mySIGhandler);
   pid_t pid = fork();
 
   // if (pid == 0) {
@@ -39,21 +38,22 @@ int main(int argc, char const *argv[]) {
   if (pid == 0) {
     printf("Child created\n");
     while (1) {
-      printf("Child waiting for SIGKILL\n");
-      while (not_done) {
-        count++;
-      }
+      sleep(1);
+      printf("Child sending SIGALRM\n");
+      kill(getppid(), SIGALRM);
     }
     printf("This line should not be printed\n");
     exit(0);
   } else {
-    sleep(4);
-    printf("Parent sending SIGKILL\n");
+    printf("Parent waiting for SIGALRM\n");
+    while (not_done) {
+      count++;
+    }
     kill(pid, SIGKILL);
+    printf("Parent sending SIGKILL\n");
     wait(NULL);
   }
 
-  printf("Parent received SIGKILL after %d iterations\n", count);
-  // printf("Parent received SIGALRM after %d iterations\n", count);
+  printf("Parent received SIGALRM after %d iterations\n", count);
   return 0;
 }
