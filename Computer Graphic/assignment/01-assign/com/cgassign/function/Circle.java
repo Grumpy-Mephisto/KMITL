feab1903552bb7f7a9d09b51f52e480a9cc943fc
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-12-19 22:10:28 +0700

    🐛 fix(cg): make can be filled or not in `Circle` function

--- Computer Graphic/assignment/01-assign/com/cgassign/function/Circle.java
+++ Computer Graphic/assignment/01-assign/com/cgassign/function/Circle.java
@@ -3,8 +3,11 @@ package com.cgassign.function;
 import java.awt.Graphics;
 
 public class Circle {
-    public void drawCircle(Graphics g, int x, int y, int radius) {
+    public void drawCircle(Graphics g, int x, int y, int radius, boolean filled) {
         int diameter = radius * 2;
         g.drawOval(x - radius, y - radius, diameter, diameter);
+        if (filled) {
+            g.fillOval(x - radius, y - radius, diameter, diameter);
+        }
     }
 }
