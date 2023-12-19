95b313b28b8aadff0bc1f1a4fb96b39712e7fe0c
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-12-19 20:32:36 +0700

    🔨 refactor(cg): make better on structure of project

--- Computer Graphic/assignment/01-assign/com/cgassign/gui/NewYearImage.java
+++ Computer Graphic/assignment/01-assign/com/cgassign/gui/NewYearImage.java
@@ -1,7 +1,26 @@
 package com.cgassign.gui;
 
+import com.cgassign.function.*;
+
 import javax.swing.*;
+import java.awt.*;
 
 public class NewYearImage extends JPanel {
-    public NewYearImage() {}
+    private Line line = new Line();
+    private Curve curve = new Curve();
+    private Triangle triangle = new Triangle();
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        // Example usage of drawing functionalities
+
+        // Drawing a line
+        line.drawBresenhamLine(g, 50, 50, 200, 100);
+        // Drawing a Bezier curve
+        curve.drawBezierCurve(g, 50, 150, 150, 250, 250, 150);
+        // Drawing a filled triangle
+        triangle.drawFillPolygonTriangle(g, 300, 50, 450, 150, 350, 250);
+    }
 }
