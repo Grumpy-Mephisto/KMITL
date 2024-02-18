adb9d4c68dba8086615f66ca177ee10ae073b849
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-02-18 20:12:36 +0700

    🐛 fix(algorithms): make better output

--- Analysis and Design of Algorithms/07-lab/SOSS/main.go
+++ Analysis and Design of Algorithms/07-lab/SOSS/main.go
@@ -1,105 +1,61 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 	"strconv"
-	"strings"
 )
 
-type Subset struct {
-	binaryRep string
-	sum       int
+type SubsetCalculator interface {
+	FindSubsets(set []int, targetSum int)
 }
 
-func NewSubset(binaryRep string, sum int) *Subset {
-	return &Subset{binaryRep: binaryRep, sum: sum}
+type SubsetCalculatorImpl struct{}
+
+func NewSubsetCalculator() *SubsetCalculatorImpl {
+	return &SubsetCalculatorImpl{}
 }
 
-func GenerateSubsets(set []int) ([]*Subset, int) {
-	subsets := make([]*Subset, 1<<len(set))
-	maxBinaryLength := len(set)
-	targetSum := 0
-
-	for i := range subsets {
-		binaryRep := fmt.Sprintf("%0*b", maxBinaryLength, i)
+func (s *SubsetCalculatorImpl) FindSubsets(set []int, targetSum int) {
+	N := 1 << uint(len(set))
+	fmt.Println("ix\tx\treverseX\tSum")
+	for ix := 0; ix < N; ix++ {
 		sum := 0
-
-		for j, bit := range binaryRep {
-			if bit == '1' {
+		for j := 0; j < len(set); j++ {
+			if (ix>>uint(j))%2 == 1 {
 				sum += set[j]
 			}
 		}
-
-		subsets[i] = NewSubset(binaryRep, sum)
-		if i < len(set) { // Only sum set elements once
-			targetSum += set[i]
+		binary := strconv.FormatInt(int64(ix), 2)
+		binary = padLeft(binary, len(set), '0')
+		reversedBinary := reverseString(binary)
+		if sum == targetSum {
+			fmt.Printf("%d\t%s\t%s\t\t%d📌\n", ix, binary, reversedBinary, sum)
+		} else {
+			fmt.Printf("%d\t%s\t%s\t\t%d\n", ix, binary, reversedBinary, sum)
 		}
 	}
-
-	return subsets, targetSum
 }
 
-func printSubsets(subsets []*Subset, targetSum int) string {
-	var sb strings.Builder
-	maxBinaryLength := len(subsets[0].binaryRep)
-	indexWidth := len(fmt.Sprintf("%d", len(subsets)-1))
-	sumWidth := len(fmt.Sprintf("%d", targetSum))
-
-	headerFormat := fmt.Sprintf("| %%-%ds | %%-%ds | %%%ds | %%-5s |\n", indexWidth, maxBinaryLength, sumWidth)
-	separator := strings.Repeat("-", len(fmt.Sprintf(headerFormat, "Index", "Subset (Binary)", "Sum", "Match")))
-
-	sb.WriteString(separator + "\n")
-	sb.WriteString(fmt.Sprintf(headerFormat, "Index", "Subset (Binary)", "Sum", "Match"))
-	sb.WriteString(separator + "\n")
-
-	for i, subset := range subsets {
-		matchIndicator := "No"
-		if subset.sum == targetSum {
-			matchIndicator = "Yes"
-		}
-		sb.WriteString(fmt.Sprintf(headerFormat, fmt.Sprintf("%d", i), subset.binaryRep, fmt.Sprintf("%d", subset.sum), matchIndicator))
+func padLeft(str string, length int, char rune) string {
+	for len(str) < length {
+		str = string(char) + str
 	}
-	sb.WriteString(separator + "\n")
-	return sb.String()
+	return str
+}
+
+func reverseString(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
 }
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
+	W := []int{11, 13, 3, 7}
+	targetSum := W[1] + W[2] + W[3]
 
-	fmt.Print("Enter set elements separated by space: ")
-	scanner.Scan()
-	input := scanner.Text()
-	inputElements := strings.Split(input, " ")
-
-	W := make([]int, len(inputElements))
-	for i, element := range inputElements {
-		num, err := strconv.Atoi(element)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error reading set element: %v\n", err)
-			os.Exit(1)
-		}
-		W[i] = num
-	}
-
-	fmt.Print("Enter target sum (Press enter for the sum of all elements): ")
-	scanner.Scan()
-	targetSumInput := scanner.Text()
-	targetSum := 0
-	if targetSumInput != "" {
-		var err error
-		targetSum, err = strconv.Atoi(targetSumInput)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error reading target sum: %v\n", err)
-			os.Exit(1)
-		}
-	}
-
-	subsets, defaultSum := GenerateSubsets(W)
-	if targetSum == 0 {
-		targetSum = defaultSum
-	}
-
-	fmt.Println(printSubsets(subsets, targetSum))
+	calculator := NewSubsetCalculator()
+	fmt.Println("Target Sum:", targetSum)
+	calculator.FindSubsets(W, targetSum)
 }
