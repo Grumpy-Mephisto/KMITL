1412a3ed012fb41e930568c423ad623696cf8f8d
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-02-19 14:11:17 +0700

    🐛 fix(cg): add interact with kb

--- Computer Graphic/lab/10-lab/main.cpp
+++ Computer Graphic/lab/10-lab/main.cpp
@@ -1,8 +1,6 @@
 #undef GLFW_DLL
 #include <iostream>
-#include <stdio.h>
-#include <string.h>
-#include <string>
+#include <vector>
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
@@ -11,41 +9,33 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-#include <cmath>
-#include <vector>
-
 #include "Libs/Mesh.h"
 #include "Libs/Shader.h"
 #include "Libs/Window.h"
 #include "Libs/stb_image.h"
 
 const GLint WIDTH = 800, HEIGHT = 600;
+const char *vShader = "Shaders/shader.vert";
+const char *fShader = "Shaders/shader.frag";
+const char *TextureFile = "Textures/uvmap.png";
+const char *ModelFile = "Models/suzanne.obj";
+const int NumberPyramid = 10;
 
-Window mainWindow;
+glm::vec3 cameraPosition(1.0f, 0.5f, 2.0f);
+glm::vec3 cameraDirection(0.0f, 0.0f, -1.0f);
+glm::vec3 upVector(0.0f, 1.0f, 0.0f);
+
+Window mainWindow(WIDTH, HEIGHT, 3, 3, "Laboratory 9th");
 std::vector<Mesh *> meshList;
 std::vector<Shader> shaderList;
-
-// Vertex Shader & Fragment Shader
-static const char *vShader = "Shaders/shader.vert";
-static const char *fShader = "Shaders/shader.frag";
-
-// Pyramid
-static const int NumberPyramid = 10;
-
-// Texture file
-static const char *TextureFile = "Textures/uvmap.png";
-
-// Model
-static const char *ModelFile = "Models/suzanne.obj";
+GLuint texture;
 
 void CreateTriangle() {
   GLfloat vertices[] = {-1.0f, -1.0f, 0.0f, 0.0f, 0.0f,  0.0,  -1.0f,
                         1.0f,  0.5f,  0.0f, 1.0f, -1.0f, 0.0f, 1.0f,
                         0.0f,  0.0f,  1.0f, 0.0f, 0.5f,  1.0f};
 
-  unsigned int indices[] = {
-      0, 3, 1, 1, 3, 2, 2, 3, 0, 0, 1, 2,
-  };
+  unsigned int indices[] = {0, 3, 1, 1, 3, 2, 2, 3, 0, 0, 1, 2};
 
   int numVertices = sizeof(vertices) / sizeof(vertices[0]);
   int numIndices = sizeof(indices) / sizeof(indices[0]);
@@ -53,9 +43,7 @@ void CreateTriangle() {
   Mesh *obj1 = new Mesh();
   obj1->CreateMesh(vertices, indices, numVertices, numIndices);
 
-  for (int i = 0; i < NumberPyramid; i++) {
-    meshList.push_back(obj1);
-  }
+  meshList.assign(NumberPyramid, obj1);
 }
 
 void CreateShaders() {
@@ -69,24 +57,13 @@ void CreateObjects() {
   bool loaded = obj1->CreateMeshFromOBJ(ModelFile);
 
   if (loaded) {
-    for (int i = 0; i < 10; i++) {
-      meshList.push_back(obj1);
-    }
+    meshList.assign(NumberPyramid, obj1);
   } else {
     std::cerr << "Failed to load object" << std::endl;
   }
 }
 
-int main() {
-  mainWindow = Window(WIDTH, HEIGHT, 3, 3, "Laboratory 9th");
-  mainWindow.initialise();
-
-  // CreateTriangle();
-  CreateObjects();
-  CreateShaders();
-
-  unsigned int texture;
-
+void InitializeTexture() {
   glGenTextures(1, &texture);
   glBindTexture(GL_TEXTURE_2D, texture);
 
@@ -101,20 +78,22 @@ int main() {
   unsigned char *data = stbi_load(TextureFile, &width, &height, &nrChannels, 0);
 
   if (data) {
-    if (nrChannels == 3) {
-      glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
-                   GL_UNSIGNED_BYTE, data);
-    } else if (nrChannels == 4) {
-      glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA,
-                   GL_UNSIGNED_BYTE, data);
-    }
-
+    GLenum format = (nrChannels == 3) ? GL_RGB : GL_RGBA;
+    glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format,
+                 GL_UNSIGNED_BYTE, data);
     glGenerateMipmap(GL_TEXTURE_2D);
   } else {
     std::cerr << "Failed to load texture" << std::endl;
   }
 
   stbi_image_free(data);
+}
+
+int main() {
+  mainWindow.initialise();
+  CreateObjects();
+  CreateShaders();
+  InitializeTexture();
 
   GLfloat bufferWidth = mainWindow.getBufferWidth();
   GLfloat bufferHeight = mainWindow.getBufferHeight();
@@ -125,12 +104,35 @@ int main() {
   }
 
   GLuint uniformModel = 0, uniformProjection = 0, uniformView = 0;
-
   glm::mat4 projection = glm::ortho(-4.0f, 4.0f, -3.0f, 3.0f, 0.1f, 100.0f);
 
   while (!mainWindow.getShouldClose()) {
     // Get + Handle user input events
     glfwPollEvents();
+
+    // Handle Keyboard Input for Camera Movement
+    const float cameraSpeed = 0.05f;
+
+    if (glfwGetKey(mainWindow.getMainWindow(), GLFW_KEY_W) == GLFW_PRESS) {
+      cameraPosition += cameraSpeed * cameraDirection;
+    }
+    if (glfwGetKey(mainWindow.getMainWindow(), GLFW_KEY_S) == GLFW_PRESS) {
+      cameraPosition -= cameraSpeed * cameraDirection;
+    }
+    if (glfwGetKey(mainWindow.getMainWindow(), GLFW_KEY_A) == GLFW_PRESS) {
+      cameraPosition -=
+          glm::normalize(glm::cross(cameraDirection, upVector)) * cameraSpeed;
+    }
+    if (glfwGetKey(mainWindow.getMainWindow(), GLFW_KEY_D) == GLFW_PRESS) {
+      cameraPosition +=
+          glm::normalize(glm::cross(cameraDirection, upVector)) * cameraSpeed;
+    }
+    if (glfwGetKey(mainWindow.getMainWindow(), GLFW_KEY_Q) == GLFW_PRESS) {
+      cameraPosition += glm::vec3(0.0f, 1.0f, 0.0f) * cameraSpeed;
+    }
+    if (glfwGetKey(mainWindow.getMainWindow(), GLFW_KEY_E) == GLFW_PRESS) {
+      cameraPosition -= glm::vec3(0.0f, 1.0f, 0.0f) * cameraSpeed;
+    }
 
     // Clear window
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
@@ -147,32 +149,8 @@ int main() {
     glm::mat4 model = glm::mat4(1.0f);
 
     // Camera
-    glm::mat4 view = glm::mat4(1.0f);
-    glm::vec3 cameraPosition = glm::vec3(1.0f, 0.5f, 2.0f);
-    glm::vec3 cameraTarget = glm::vec3(0.0f, -0.3f, -1.0f);
-    glm::vec3 upVector = glm::vec3(0.0f, 1.0f, 0.0f);
-
-    glm::vec3 cameraDirection = glm::normalize(cameraPosition - cameraTarget);
-    glm::vec3 cameraRight =
-        glm::normalize(glm::cross(upVector, cameraDirection));
-    glm::vec3 cameraUp = glm::cross(cameraDirection, cameraRight);
-
-    glm::mat4 cameraPositionMatrix = glm::mat4(1.0f);
-    cameraPositionMatrix[3][0] = -cameraPosition.x;
-    cameraPositionMatrix[3][1] = -cameraPosition.y;
-    cameraPositionMatrix[3][2] = -cameraPosition.z;
-
-    glm::mat4 cameraRotationMatrix = glm::mat4(1.0f);
-    cameraRotationMatrix[0] =
-        glm::vec4(cameraRight.x, cameraUp.x, cameraDirection.x, 0.0f);
-    cameraRotationMatrix[1] =
-        glm::vec4(cameraRight.y, cameraUp.y, cameraDirection.y, 0.0f);
-    cameraRotationMatrix[2] =
-        glm::vec4(cameraRight.z, cameraUp.z, cameraDirection.z, 0.0f);
-
-    view = cameraRotationMatrix * cameraPositionMatrix;
-
-    view = glm::lookAt(cameraPosition, cameraTarget, upVector);
+    glm::mat4 view =
+        glm::lookAt(cameraPosition, cameraPosition + cameraDirection, upVector);
 
     model = glm::translate(model, glm::vec3(0.3f, 0.0f, -2.5f));
     model = glm::rotate(model, 90.0f * 3.1416f / 180.0f,
@@ -182,6 +160,7 @@ int main() {
     glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
     glUniformMatrix4fv(uniformProjection, 1, GL_FALSE,
                        glm::value_ptr(projection));
+    glUniformMatrix4fv(uniformView, 1, GL_FALSE, glm::value_ptr(view));
 
     glm::vec3 pyramidPositions[] = {
         glm::vec3(0.0f, 0.0f, -2.5f),   glm::vec3(2.0f, 5.0f, -15.0f),
@@ -191,8 +170,6 @@ int main() {
         glm::vec3(1.5f, 0.2f, -1.5f),   glm::vec3(-1.3f, 1.0f, -1.5f)};
 
     for (int i = 0; i < NumberPyramid; i++) {
-      glUniformMatrix4fv(uniformView, 1, GL_FALSE, glm::value_ptr(view));
-
       glm::mat4 model = glm::mat4(1.0f);
 
       model = glm::translate(model, pyramidPositions[i]);
@@ -209,8 +186,6 @@ int main() {
 
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, texture);
-    meshList[0]->RenderMesh();
-
     meshList[0]->RenderMesh();
 
     glUseProgram(0);
