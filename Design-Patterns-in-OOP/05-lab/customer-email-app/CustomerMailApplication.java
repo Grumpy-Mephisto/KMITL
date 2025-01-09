425a49993c62cfcdc1ed4bd600f9ecafc13cf4ab
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2025-01-10 00:00:04 +0700

    style(oop): improve code formatting and whitespace consistency in customer email classes

--- Design-Patterns-in-OOP/05-lab/customer-email-app/CustomerMailApplication.java
+++ Design-Patterns-in-OOP/05-lab/customer-email-app/CustomerMailApplication.java
@@ -1,18 +1,21 @@
 import java.util.Scanner;
+
 public class CustomerMailApplication {
     /**
      * @param args the command line arguments
      */
     private Customer customer;
+
     public CustomerMailApplication(Customer customer) {
         this.customer = customer;
     }
+
     public static String getCustomerTypeFromUser() {
         String customerType = null;
         Scanner inp = new Scanner(System.in);
         System.out.print("Please choose customer type 1. Regular, 2. Mountain, 3. Delinquent ");
         int type = inp.nextInt();
-        switch(type) {
+        switch (type) {
             case 1:
                 customerType = "Regular";
                 break;
@@ -26,14 +29,15 @@ public class CustomerMailApplication {
         inp.close();
         return customerType;
     }
+
     public String generateMail() {
         return customer.createMail();
     }
-    
+
     public static void main(String[] args) {
         String customerType = getCustomerTypeFromUser();
-        Customer customer  = null;
-        switch(customerType) {
+        Customer customer = null;
+        switch (customerType) {
             case "Regular":
                 customer = new RegularCustomer();
                 break;
@@ -42,9 +46,9 @@ public class CustomerMailApplication {
                 break;
             case "Delinquent":
                 customer = new DelinquentCustomer();
-                break; 
+                break;
         }
         CustomerMailApplication app = new CustomerMailApplication(customer);
-        System.out.println(app.generateMail());        
+        System.out.println(app.generateMail());
     }
 }
